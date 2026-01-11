a51e4b70d37956aaf0e37c1213522c014659c384
Restu <restujati.saputro@bisnis.pnj.ac.id>
2026-01-12 00:33:18 +0700

    upload file

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$hst = 'escore-mice.com';
+$hst = 'localhost';
 $usrn = 'escore-mice';
 $pwds = 'uVHAxf9lPFsOShSnSnjWfZ6Bt';
 $dbse = 'escore-mice';
